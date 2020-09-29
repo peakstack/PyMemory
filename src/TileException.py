da1d451e08154b9d1204556c2e27094c7e85eead
peakstack <43585195+peakstack@users.noreply.github.com>
2020-09-29 09:07:29 +0200

    Delete TileException.py

--- src/TileException.py
+++
@@ -1,9 +0,0 @@
-class TileException(Exception):
-    """Exception raised for invalid tiles."""
-
-    def __init__(self, message="Tile error."):
-        self.message = message
-        super().__init__(self.message)
-
-    def __str__(self):
-        return f'{self.message}'
